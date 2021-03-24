172be3e45451986ef523084cfb1f53cb90276169
Shivani Aggarwal <shivani.aggarwal2422@gmail.com>
2021-03-24 18:16:26 -0400

    objects in React

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-
+let city = {
+  name: "London",
+  country: "UK"
+}
 ReactDOM.render(
-  React.createElement("h1", { style: { color: "red" } }, "Hello!")
+  <h1> {city.name} is in {city.country}</h1>
   , document.getElementById('root')
 );
 

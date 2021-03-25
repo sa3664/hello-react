33ebac967d97176ea0807d89db44cc022bdfb440
Shivani Aggarwal <shivani.aggarwal2422@gmail.com>
2021-03-24 23:42:48 -0400

    React Components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-function Hello(props) {
-  console.log(props);
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+function Lake({ name }) {
   return (
     <div>
-      <h1> Welcome to {props.library} !</h1>
-      <p>{props.message}</p>
+      <h1>Visit {name}!</h1>
+    </div>
+  );
+}
+
+function SkiResort({ name }) {
+  return (
+    <div>
+      <h1>Visit {name}!</h1>
+    </div>
+  );
+}
+
+function App(props) {
+  return (
+    <div>
+      {props.season === "summer" ? (
+        <Lake name="Jenny Lake" />
+      ) : props.season === "winter" ? (
+        <SkiResort name="JHMR" />
+      ) : (
+        <h1>
+          Come back in the winter or summer!
+        </h1>
+      )}
     </div>
   );
 }
 
 ReactDOM.render(
-  <Hello library="React" message="Have Fun! " />, document.getElementById('root')
+  <App season="fall" />,
+  document.getElementById("root")
 );
-
-
